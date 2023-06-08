dbad42b2407eba5794abe2edc7aa5856d00652b8
QueenterJuma <jumahqueenter@gmail.com>
2023-06-08 16:05:14 -0700

    Add after_save in like.rb

--- app/models/like.rb
+++ app/models/like.rb
@@ -2,7 +2,11 @@ class Like
   belongs_to :author, class_name: 'User'
   belongs_to :post, counter_cache: :likes_counter
 
-  def update_like_counter
+  after_save :update_likes_counter
+
+  private
+
+  def update_likes_counter
     post.increment!(:likes_counter)
   end
 end
