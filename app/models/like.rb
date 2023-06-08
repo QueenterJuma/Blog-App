22e9c489fa672388a4a58ef43a360fc2020cec0e
QueenterJuma <jumahqueenter@gmail.com>
2023-06-08 11:49:39 -0700

    Fixed linters

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,8 +1,8 @@
 class Like
-    belongs_to :author, class_name: 'User'
-    belongs_to :post, counter_cache: :likes_counter
-    
-    def update_like_counter
-        post.increment!(:likes_counter)
-    end
+  belongs_to :author, class_name: 'User'
+  belongs_to :post, counter_cache: :likes_counter
+
+  def update_like_counter
+    post.increment!(:likes_counter)
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,10 +1,10 @@
 class Post < ApplicationRecord
-    belongs_to :user
-    belongs_to :author, class_name: 'User', counter_cache: :post_counter
-    has_many :comment
-    has_many :like
+  belongs_to :user
+  belongs_to :author, class_name: 'User', counter_cache: :post_counter
+  has_many :comment
+  has_many :like
 
-    def best_five_comment
-        comment.order(created_at: :desc).limit(5)
-    end
+  def best_five_comment
+    comment.order(created_at: :desc).limit(5)
+  end
 end
