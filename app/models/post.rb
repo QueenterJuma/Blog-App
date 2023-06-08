e7909250bbfc3d555fbfcc91a0ddcea6e75a8391
QueenterJuma <jumahqueenter@gmail.com>
2023-06-08 16:05:32 -0700

    Add after_save in post.rb

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,10 +1,16 @@
 class Post < ApplicationRecord
-  belongs_to :user
-  belongs_to :author, class_name: 'User', counter_cache: :post_counter
-  has_many :comment
-  has_many :like
+  belongs_to :author, class_name: 'User'
+  has_many :comments, :likes
 
-  def best_five_comment
-    comment.order(created_at: :desc).limit(5)
+  after_save :update_posts_counter
+
+  def best_five_comments
+    commenst.order(created_at: :desc).limit(5)
+  end
+
+  private
+  
+  def update_posts_counter
+    author.increment!(:posts_counter)
   end
 end
