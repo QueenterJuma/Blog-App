be83efe641ed38460d26400e6c868c1d2b34715b
QueenterJuma <jumahqueenter@gmail.com>
2023-06-08 11:41:35 -0700

    Add comment.rb

---
+++ app/models/comment.rb
@@ -0,0 +1,8 @@
+class Comment
+    belongs_to :author, class_name: 'User'
+    belongs_to :post, counter_cache: :comments_counter
+    
+    def update_comment_counter
+        post.increment!(:comments_counter)
+    end
+end
