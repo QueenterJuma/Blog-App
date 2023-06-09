2eed415da5346d0f58a9b47a2e325610a8fa31e3
QueenterJuma <jumahqueenter@gmail.com>
2023-06-09 11:34:40 -0700

    Add comment_spec.rb

---
+++ spec/models/comment_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe Comment, type: :model do
+  let(:author) { User.new(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.') }
+  let(:post) { Post.new(author: author, title: 'Nature', text: 'I love this!') }
+  subject { Comment.new(post: post, author: author, text: 'It is nice') }
+  before { subject.save }
+
+  context 'return updated comments counter' do
+      it 'increments the post\'s comments_counter on save' do
+        expect { subject.save }.to change { post.reload.comments_counter }.by(1)
+      end
+    end
+end
