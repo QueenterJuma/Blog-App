273a325fb8ec9c4e3233bfdc8f332c1d3bc0ac8d
QueenterJuma <jumahqueenter@gmail.com>
2023-06-16 22:40:31 -0700

    Fixed linters

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,7 +2,7 @@ class LikesController < ApplicationController
   def create
     post = Post.find(params[:post_id])
     @like = post.like.new(author: current_user)
-    puts "Queen"
+    puts 'Queen'
     if @like.save
       puts post.id
       redirect_to user_post_path(post.author, post)
