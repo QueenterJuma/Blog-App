3c6ac4752d262adb0fdc4e8eab09cf3d5279960b
Stephen-Adom <stephen.alaska94@hotmail.com>
2023-06-20 09:13:15 +0000

    add test for user list page

--- test/integration/users_test.rb
+++ test/integration/users_test.rb
@@ -13,4 +13,19 @@ class UsersTest < ActionDispatch::IntegrationTest
   test 'display a user profile photos' do
     assert page.has_css?("img[src='#{@user.photo}']")
   end
+
+  test 'displays the number of posts for each user' do
+    assert page.has_content?("Number of posts: #{@user.posts_counter}")
+  end
+
+  # test "redirects to the user's show page" do
+  #   click_link_or_button('user-content-details')
+  #   assert_current_path user_path(@user)
+  # end
+
+  test "redirects to the user's show page" do
+    # assert page.has_css?('a.user-content-details', wait: 5)
+    click_link("#{@user.id}")
+    assert_current_path user_path(@user)
+  end
 end
