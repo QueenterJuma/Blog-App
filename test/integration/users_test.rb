fc691e1dba9849f375b49988ba2096a112a4ee0d
Stephen-Adom <stephen.alaska94@hotmail.com>
2023-06-20 09:46:44 +0000

    add test for user details page

--- test/integration/users_test.rb
+++ test/integration/users_test.rb
@@ -18,14 +18,8 @@ class UsersTest < ActionDispatch::IntegrationTest
     assert page.has_content?("Number of posts: #{@user.posts_counter}")
   end
 
-  # test "redirects to the user's show page" do
-  #   click_link_or_button('user-content-details')
-  #   assert_current_path user_path(@user)
-  # end
-
   test "redirects to the user's show page" do
-    # assert page.has_css?('a.user-content-details', wait: 5)
-    click_link("#{@user.id}")
+    click_link(@user.id.to_s)
     assert_current_path user_path(@user)
   end
 end
