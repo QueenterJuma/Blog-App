805064db8462d0915f0cc57ee5865b4db40d1879
QueenterJuma <jumahqueenter@gmail.com>
2023-06-20 11:04:57 -0700

    Add update in post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,6 @@
 class PostsController < ApplicationController
   def index
-    @user = User.find(params[:user_id])
-    @posts = Post.where(author_id: params[:user_id])
+    @user = User.includes(post:[author,{comment: [:author]}].find(params[:user_id]))
   end
 
   def show
