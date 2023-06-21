71a6621200688d2326c21e813bb4ac8795273cbb
Stephen-Adom <stephen.alaska94@hotmail.com>
2023-06-21 13:03:17 +0000

    add destroy to post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -24,7 +24,13 @@ class PostsController < ApplicationController
   end
 
   def destroy
-    puts params[:id]
+    @post = Post.find(params[:id])
+
+    respond_to do |format|
+      if @post.destroy
+        format.html { redirect_to user_posts_path(current_user), notice: 'Post was successfully deleted.' }
+      end
+    end
   end
 
   private
