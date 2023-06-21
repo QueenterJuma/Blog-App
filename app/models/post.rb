f6f21f1f39daf02914a18fa319542046ea2cc997
Stephen-Adom <stephen.alaska94@hotmail.com>
2023-06-21 14:15:42 +0000

    update relations on post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
-  has_many :comment
+  has_many :comment, dependent: :destroy
   has_many :like
 
   validates :title, presence: true, length: { maximum: 250 }
