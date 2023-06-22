684f6b44d9402fb72afb2f0d856fc4d753031039
QueenterJuma <jumahqueenter@gmail.com>
2023-06-22 15:46:51 -0700

    Fixed linters

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,14 +1,12 @@
-# frozen_string_literal: true
-
 class Ability
   include CanCan::Ability
 
   def initialize(user)
     if user.is? :admin
-      can :destroy, [Post, Comment]   
-    else 
-      can :destroy, [Post, Comment], 
-      author_id: user.id
+      can :destroy, [Post, Comment]
+    else
+      can :destroy, [Post, Comment],
+          author_id: user.id
     end
   end
 end
